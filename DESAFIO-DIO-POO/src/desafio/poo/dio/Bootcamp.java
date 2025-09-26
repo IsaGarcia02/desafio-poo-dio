cd97d7abc24958bc16d096039e41d325ae3e5112
IsaGarcia02 <garciaisabella.c02@gmail.com>
2025-09-25 22:27:03 -0300

    Commit inicial

---
+++ DESAFIO-DIO-POO/src/desafio/poo/dio/Bootcamp.java
@@ -0,0 +1,70 @@
+package desafio.poo.dio;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.time.LocalDate;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+
+    private Set<Dev> devs = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevs() {
+        return devs;
+    }
+
+    public void setDevs(Set<Dev> devs) {
+        this.devs = devs;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+}
